f444704ec3a62f10727d5b52058d59420d7bdc09
Muhammad Faizan Khan <242825@students.au.edu.pk>
2026-07-04 18:26:20 +0500

    Delete public_html directory

--- public_html/resources/views/admin/components/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-  <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0">
-    <div class="flex items-center justify-start space-x-3">
-      <div>
-        © {{ now()->year}}
-      </div>
-    </div>
-  </div>
-</footer>
