dfaccfedcc27c69e0c6dad406fdce47fddde1344
Muhammad Faizan Khan <242825@students.au.edu.pk>
2026-07-04 18:26:54 +0500

    Delete public_html_frontend directory

--- public_html_frontend/mailer/forms_settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/* Forms settings */
-	$subj = "New message from the site 'ITSulu - Technology & IT Solutions Template'"; //letter subject
-	$to = 'your_email@domain.com'; // Enter Your E-mail
-	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'ITSulu'; // Your company name
-	$charset = 'UTF-8';   
-?>
